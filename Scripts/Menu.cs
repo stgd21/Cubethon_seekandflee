f88e2e4e70b6744b9e37faea6f0aea77755a9a1f
stgd21 <45217325+stgd21@users.noreply.github.com>
2020-01-11 17:33:56 -0700

    Add files via upload

---
+++ Scripts/Menu.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Menu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        //Load Scenes Sequentially via build indices
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
---
+++ Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //Physics Component of player cube
+    public Rigidbody rb;
+    //Forces to set in inspector
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+    // FixedUpdate is called evenly across framerates
+    void FixedUpdate()
+    {
+        //Add force to propel player forward
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            //Move the player right when D key is pushed
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0,
+                ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("a"))
+        {
+            //Move the player left when A key is pushed
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0,
+                ForceMode.VelocityChange);
+        }
+        if (rb.position.y < -1f)
+        {
+            //End the game if the player goes off the edge
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
